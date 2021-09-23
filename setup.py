e4ad0c5356750193c91c9db3ecaa971dac3a8b72
John Clyde <john.clyde@mozartdata.com>
2021-09-22 21:03:07 -0700

    Unpin the requirements.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='tap-copper',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_copper'],
       install_requires=[
-          'tap-framework==0.0.4',
+          'tap-framework>=0.0.4',
       ],
       extras_require={
         'dev': [
